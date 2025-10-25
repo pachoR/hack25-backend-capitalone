5893fd688da1523aefbbccf55d52a15c14e27f21
Piponsio013 <pipe1910@live.com.mx>
2025-10-25 02:36:03 -0600

    Added cors config

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import 'dotenv/config';
 import express, { Application, Request, Response } from 'express';
 import { getUsers } from './controllers/userController';
 import purchaseRoutes from './routes/purchaseRoutes';
+import { corsMiddleware } from './middleware/cors.middleware';
 
 const app: Application = express();
 const port: number = 8080;
 
+// Middlewares
+app.use(corsMiddleware);
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
