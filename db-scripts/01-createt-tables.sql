2820307038d54a4a7a952f9917b61a97513f0f7c
pacho ruiz <alfrrugu@hotmail.com>
2025-10-25 12:57:42 -0600

    feat(create-purchase): saving the purchase to the db

--- db-scripts/01-createt-tables.sql
+++ db-scripts/01-createt-tables.sql
@@ -1,7 +1,8 @@
 create table if not exists users (
    client_id   varchar(255),
    purchase_id varchar(255),
-   invest_id   varchar(255)
+   invest_id   varchar(255),
+   exec_status int
 );
 
 create table if not exists user_rules (
