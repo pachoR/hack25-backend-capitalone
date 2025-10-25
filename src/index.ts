1b60f1f4dcd76c8e3887423479c2f670ff4e5d5b
Piponsio013 <pipe1910@live.com.mx>
2025-10-25 00:57:44 -0600

    remove comments

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import purchaseRoutes from './routes/purchaseRoutes';
 const app: Application = express();
 const port: number = 8080;
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
@@ -15,7 +14,6 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/users', getUsers);
 
-// Purchase routes
 app.use('/', purchaseRoutes);
 
 app.listen(port, () => {
